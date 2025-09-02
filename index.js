24e1393ff2c4892b54284c6d3138a09fad6265e8
Abdul Hameed T A <hameedamithattuparambil@gmail.com>
2025-09-02 14:31:21 +0530

    refactored with router

--- index.js
+++ index.js
@@ -32,58 +32,20 @@ async function saveDataToFile() {
     }
 }
 
+const dbDependencies = {
+    todos,
+    nextId,
+    saveDataToFile
+};
+
+const createTodoRouter = require('./routes/todos.js');
+const todoRoutes = createTodoRouter(dbDependencies);
+
+app.use('/todos', todoRoutes);
+
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.send('Welcome to the Express API!');
 });
-
-app.get('/todos', (req, res) => {
-    res.json(todos);
-});
-
-app.post('/todos', async (req, res) => {
-    const newTodoData = req.body;
-    if(!newTodoData.text || typeof newTodoData.text !== 'string' || newTodoData.text.trim() === '') {
-        return res.status(400).json({ message: 'Validation Error: The "Text" field is required.' });
-    }
-    const newTodo = {
-        id: nextId++,
-        text: newTodoData.text.trim(),
-        completed: newTodoData.completed === true
-    };
-    todos.push(newTodo);
-    await saveDataToFile();
-    res.status(201).json(newTodo);
-});
-
-app.put('/todos/:id', async (req, res) => {
-    const idToUpdate = parseInt(req.params.id);
-    const newData = req.body;
-    const indexToUpdate = todos.findIndex(todo => todo.id === idToUpdate);
-    if(indexToUpdate === -1) {
-        return res.status(404).json({ message: 'Todo is not found' });
-    }
-    if (
-        (newData.text !== undefined && (typeof newData.text !== 'string' || newData.text.trim() === '')) ||
-        (newData.completed !== undefined && typeof newData.completed !== 'boolean')
-    ) {
-        return res.status(400).json({ message: 'Validation Error: Invalid data provided.' });
-    }
-    const updatedTodo = { ...todos[indexToUpdate], ...newData };
-    todos[indexToUpdate] = updatedTodo;
-    await saveDataToFile();
-    res.json(updatedTodo)
-})
-
-app.delete('/todos/:id', async (req, res) => {
-    const idToDelete = parseInt(req.params.id);
-    const indexToRemove = todos.findIndex(todo => todo.id === idToDelete);
-    if (indexToRemove === -1) {
-        return res.status(404).json({ message: 'Todo not found' });
-    }
-    todos.splice(indexToRemove, 1);
-    await saveDataToFile();
-    res.sendStatus(204);
-})
 
 app.listen(PORT, () => {
     console.log(`Server running on http:/localhost:${PORT}`);
